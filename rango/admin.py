1576a8a922206084fc934190bf7f4c0c8a0527bf
Iona Houston <3082512H@student.gla.ac.uk>
2026-02-26 15:37:50 +0000

    Final chapter 5 implementation

--- rango/admin.py
+++ rango/admin.py
@@ -6,6 +6,6 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 # Register your models here.
